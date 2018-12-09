6a037bca9441d628f75c4569e0bcf860feb129b9
Guillaume <garciaguillaume18@gmail.com>
2018-12-09 18:58:52 +0100

    +

--- js/main.js
+++ js/main.js
@@ -2,19 +2,22 @@ $(document).ready(function () {
 
     console.log("main.js is ready ");
 
+    //Au changement de valeur générer les champs
     $("#Inputs").bind('keyup mouseup', function () {
         GenerateFields($(this).val());
     });
 
 })
 
-function GenerateFields(number) {
+function GenerateFields(n) {
 
     var div = $('#Generated_Fields');
 
+    //Vider d'abord
     div.html('');
 
-    for (var i = 0; i < number; i++) {
+    //Générer n champs
+    for (var i = 0; i < n; i++) {
         div.append('<div class="contrainte"><label for="Inputs">Contrainte ' + (i + 1) + '</label><input class="form-control" type="text" id="contrainte' + (i + 1) + '"></div>');
     }
 
