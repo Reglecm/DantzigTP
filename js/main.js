abfa3069b393076dbe501cacb0449abb88a694b8
Guillaume <garciaguillaume18@gmail.com>
2018-12-09 18:51:52 +0100

    Front Generation dynamique du nombre de contraintes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-console.log("main.js is ready ");
- 
+$(document).ready(function () {
+
+    console.log("main.js is ready ");
+
+    $("#Inputs").bind('keyup mouseup', function () {
+        GenerateFields($(this).val());
+    });
+
+})
+
+function GenerateFields(number) {
+
+    var div = $('#Generated_Fields');
+
+    div.html('');
+
+    for (var i = 0; i < number; i++) {
+        div.append('<div class="contrainte"><label for="Inputs">Contrainte ' + (i + 1) + '</label><input class="form-control" type="text" id="contrainte' + (i + 1) + '"></div>');
+    }
+
+}
